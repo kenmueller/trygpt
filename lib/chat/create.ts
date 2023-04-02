8d3ee1829f56b57d3f3f7551c7391ea3c30f48e8
Ken Mueller <kenmueller0@gmail.com>
2023-04-02 13:08:09 -0700

    create chat

--- lib/chat/create.ts
+++ lib/chat/create.ts
@@ -1,7 +1,30 @@
 import 'server-only'
 
-import User from '@/lib/user'
+import { sql, DatabasePoolConnection } from 'slonik'
 
-const createChat = (user: User) => Promise.resolve('')
+import User from '@/lib/user'
+import { connect } from '@/lib/pool'
+import { nanoid } from 'nanoid'
+
+const createChat = (user: User, connection?: DatabasePoolConnection) =>
+	connection
+		? createChatWithConnection(user, connection)
+		: connect(connection => createChatWithConnection(user, connection))
+
+const createChatWithConnection = async (
+	user: User,
+	connection: DatabasePoolConnection
+) => {
+	const id = nanoid()
+
+	await connection.query(
+		sql.unsafe`INSERT INTO
+				   chats (user_id, id)
+				   VALUES (${user.id}, ${id})
+				   ON CONFLICT (id) DO NOTHING`
+	)
+
+	return id
+}
 
 export default createChat
