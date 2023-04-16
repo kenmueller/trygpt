c0153ad589ae793484e1841894724335090e2e12
Ken Mueller <kenmueller0@gmail.com>
2023-04-16 08:46:06 -0700

    font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import cx from 'classnames'
 
 import userFromRequest from '@/lib/user/fromRequest'
 import UpdateUser from '@/components/UpdateUser'
@@ -9,7 +11,30 @@ import FontAwesomeConfig from '@/components/FontAwesomeConfig'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import './layout.scss'
 
-const inter = Inter({ subsets: ['latin'], display: 'swap' })
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['400', '700', '900'],
+	fallback: [
+		'system-ui',
+		'-apple-system',
+		'BlinkMacSystemFont',
+		'Segoe UI',
+		'Roboto',
+		'Oxygen',
+		'Ubuntu',
+		'Cantarell',
+		'Open Sans',
+		'Helvetica Neue',
+		'sans-serif'
+	]
+})
+
+const sfMono = localFont({
+	variable: '--font-sf-mono',
+	src: '../assets/SFMono-Regular.otf',
+	weight: '400',
+	fallback: ['Consolas', 'Liberation Mono', 'Menlo', 'Courier', 'monospace']
+})
 
 export const metadata = BASE_METADATA
 
@@ -18,7 +43,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
 
 	return (
 		<html lang="en" dir="ltr">
-			<body className={inter.className}>
+			<body className={cx(inter.className, sfMono.variable)}>
 				{children}
 				<UpdateUser user={user} />
 				<FontAwesomeConfig />
