d17117ebcd8e63b3c157773941484ba97e79d2d7
Ken Mueller <kenmueller0@gmail.com>
2023-04-24 09:19:31 -0700

    using url

--- app/api/stripe/checkout-sessions/route.ts
+++ app/api/stripe/checkout-sessions/route.ts
@@ -1,9 +1,6 @@
 if (!process.env.STRIPE_INITIAL_TOKENS_PRICE_ID)
 	throw new Error('Missing STRIPE_INITIAL_TOKENS_PRICE_ID')
 
-if (!process.env.NEXT_PUBLIC_ORIGIN)
-	throw new Error('Missing NEXT_PUBLIC_ORIGIN')
-
 import { NextRequest, NextResponse } from 'next/server'
 
 import errorFromUnknown from '@/lib/error/fromUnknown'
@@ -11,15 +8,18 @@ import stripe from '@/lib/stripe'
 import userFromRequest from '@/lib/user/fromRequest'
 import HttpError from '@/lib/error/http'
 import ErrorCode from '@/lib/error/code'
+import getUrl from '@/lib/getUrl'
 
 export const dynamic = 'force-dynamic'
 
 export const POST = async (_request: NextRequest) => {
 	try {
+		const url = getUrl()
+
 		const user = await userFromRequest()
 		if (!user) throw new HttpError(ErrorCode.Unauthorized, 'Unauthorized')
 
-		const { url } = await stripe.checkout.sessions.create({
+		const { url: checkoutUrl } = await stripe.checkout.sessions.create({
 			line_items: [
 				{
 					price: process.env.STRIPE_INITIAL_TOKENS_PRICE_ID!,
@@ -30,14 +30,14 @@ export const POST = async (_request: NextRequest) => {
 			customer: user.customerId,
 			mode: 'payment',
 			payment_intent_data: { setup_future_usage: 'off_session' },
-			success_url: `${process.env.NEXT_PUBLIC_ORIGIN!}/chats/new`,
-			cancel_url: `${process.env.NEXT_PUBLIC_ORIGIN!}/chats/new`
+			success_url: `${url.origin}/chats/new`,
+			cancel_url: `${url.origin}/chats/new`
 		})
 
-		if (!url)
+		if (!checkoutUrl)
 			throw new HttpError(ErrorCode.Internal, 'Missing checkout session URL')
 
-		return new NextResponse(url)
+		return new NextResponse(checkoutUrl)
 	} catch (unknownError) {
 		const { code, message } = errorFromUnknown(unknownError)
 		return new NextResponse(message, { status: code })
