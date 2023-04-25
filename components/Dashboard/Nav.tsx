d907924f6319a27a9aba1f13f8b2341b810f46d2
Ken Mueller <kenmueller0@gmail.com>
2023-04-25 11:26:45 -0700

    nav styles

--- components/Dashboard/Nav.tsx
+++ components/Dashboard/Nav.tsx
@@ -22,15 +22,19 @@ const DashboardNav = ({
 	}, [setIsSidebarShowing])
 
 	return (
-		<nav className="flex justify-between items-center px-4 py-2 w-1000:hidden">
+		<nav className="flex justify-between items-center w-screen px-4 py-2 w-1000:hidden">
 			<button className="shrink-0" onClick={showSidebar}>
 				<FontAwesomeIcon icon={faBars} />
 			</button>
-			<h1 className="mx-4">{children}</h1>
-			{canCreateChat && (
+			<h1 className="mx-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
+				{children}
+			</h1>
+			{canCreateChat ? (
 				<Link className="shrink-0" href="/chats/new">
 					<FontAwesomeIcon icon={faPlus} />
 				</Link>
+			) : (
+				<span className="w-[14px]" aria-hidden />
 			)}
 		</nav>
 	)
