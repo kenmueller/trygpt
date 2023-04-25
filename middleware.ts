670429f7cb2f163e5365c090ae7c656137403818
Ken Mueller <kenmueller0@gmail.com>
2023-04-25 12:20:38 -0700

    production origin

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
+import DEV from './lib/dev'
 
 const middleware = (request: NextRequest) => {
 	const headers = new Headers(request.headers)
+	const url = new URL(request.url)
 
-	headers.set('x-url', request.url)
+	headers.set(
+		'x-url',
+		new URL(url.pathname, DEV ? url.origin : 'https://trygpt.co').href
+	)
 
 	return NextResponse.next({
 		request: { headers }
