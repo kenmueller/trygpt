48351d319f5bf863ad4282a3121ae09ed7a80438
Ken Mueller <kenmueller0@gmail.com>
2023-04-25 11:58:18 -0700

    bigger nav buttons

--- components/Dashboard/Nav.tsx
+++ components/Dashboard/Nav.tsx
@@ -22,19 +22,19 @@ const DashboardNav = ({
 	}, [setIsSidebarShowing])
 
 	return (
-		<nav className="flex justify-between items-center w-screen px-4 py-2 w-1000:hidden">
-			<button className="shrink-0" onClick={showSidebar}>
+		<nav className="flex justify-between items-center w-screen w-1000:hidden">
+			<button className="shrink-0 px-4 py-2 text-xl" onClick={showSidebar}>
 				<FontAwesomeIcon icon={faBars} />
 			</button>
 			<h1 className="mx-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
 				{children}
 			</h1>
 			{canCreateChat ? (
-				<Link className="shrink-0" href="/chats/new">
+				<Link className="shrink-0 px-4 py-2 text-xl" href="/chats/new">
 					<FontAwesomeIcon icon={faPlus} />
 				</Link>
 			) : (
-				<span className="w-[14px]" aria-hidden />
+				<span className="shrink-0 px-4 py-2 w-[17.5px]" aria-hidden />
 			)}
 		</nav>
 	)
