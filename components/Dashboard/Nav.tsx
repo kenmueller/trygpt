654f1fba42f432cf302fbdeeb15a9bf44331122a
Ken Mueller <kenmueller0@gmail.com>
2023-04-25 15:46:39 -0700

    opacity

--- components/Dashboard/Nav.tsx
+++ components/Dashboard/Nav.tsx
@@ -23,14 +23,20 @@ const DashboardNav = ({
 
 	return (
 		<nav className="flex justify-between items-center w-screen w-1000:hidden">
-			<button className="shrink-0 px-4 py-2 text-xl" onClick={showSidebar}>
+			<button
+				className="shrink-0 px-4 py-2 text-xl transition-opacity ease-linear hover:opacity-70"
+				onClick={showSidebar}
+			>
 				<FontAwesomeIcon icon={faBars} />
 			</button>
 			<h1 className="mx-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
 				{children}
 			</h1>
 			{canCreateChat ? (
-				<Link className="shrink-0 px-4 py-2 text-xl" href="/chats/new">
+				<Link
+					className="shrink-0 px-4 py-2 text-xl transition-opacity ease-linear hover:opacity-70"
+					href="/chats/new"
+				>
 					<FontAwesomeIcon icon={faPlus} />
 				</Link>
 			) : (
