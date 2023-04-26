8eea722334e54ef43a3ea2342a023e6f9e690336
Ken Mueller <kenmueller0@gmail.com>
2023-04-26 12:11:19 -0700

    csp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
+if (!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID)
+	throw new Error('Missing NEXT_PUBLIC_FIREBASE_PROJECT_ID')
+
+const { getCSP, SELF, UNSAFE_INLINE, UNSAFE_EVAL } = require('csp-header')
 const { IgnorePlugin } = require('webpack')
+
+const DEV = process.env.NODE_ENV === 'development'
+
+const csp = getCSP({
+	directives: {
+		'base-uri': [SELF],
+		'default-src': [SELF],
+		'style-src': [SELF, UNSAFE_INLINE],
+		'script-src': [
+			SELF,
+			UNSAFE_INLINE,
+			...(DEV ? [UNSAFE_EVAL] : []),
+			'https://apis.google.com'
+		],
+		'connect-src': [SELF, 'https://identitytoolkit.googleapis.com'],
+		'frame-src': [
+			SELF,
+			`https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`
+		]
+	}
+})
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -17,6 +42,12 @@ const config = {
 			'lh6.googleusercontent.com'
 		]
 	},
+	headers: async () => [
+		{
+			source: '/:path*',
+			headers: [{ key: 'content-security-policy', value: csp }]
+		}
+	],
 	webpack: config => {
 		config.plugins.push(new IgnorePlugin({ resourceRegExp: /^pg-native$/ }))
 		return config
