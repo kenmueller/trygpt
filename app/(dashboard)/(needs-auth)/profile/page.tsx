4dbbd2b6540823e08ac1b5b6954c2be9b3149663
Ken Mueller <kenmueller0@gmail.com>
2023-04-28 15:28:07 -0700

    profile usage this period more info

--- app/(dashboard)/(needs-auth)/profile/page.tsx
+++ app/(dashboard)/(needs-auth)/profile/page.tsx
@@ -4,7 +4,7 @@ import {
 	COST_PER_1000_PROMPT_TOKENS,
 	COST_PER_1000_COMPLETION_TOKENS
 } from '@/lib/costPerToken'
-import costThisPeriod from '@/lib/user/costThisPeriod'
+import costThisPeriod, { baseCostThisPeriod } from '@/lib/user/costThisPeriod'
 import formatDate from '@/lib/date/format'
 import nextMonth from '@/lib/date/nextMonth'
 import formatCents from '@/lib/cents/format'
@@ -22,6 +22,18 @@ const ProfilePage = async () => {
 	const user = await userFromRequest()
 	if (!user) return null
 
+	const baseCost = baseCostThisPeriod(user)
+	const cost = costThisPeriod(user)
+
+	const nextCharge = (() => {
+		if (!user.lastCharged) return null
+
+		const minNextCharge = nextMonth(user.lastCharged)
+		const now = new Date()
+
+		return minNextCharge > now ? minNextCharge : now
+	})()
+
 	return (
 		<>
 			<Nav>Profile</Nav>
@@ -29,23 +41,42 @@ const ProfilePage = async () => {
 				<h1 className="text-4xl font-bold">Profile</h1>
 				<p>Name: {user.name}</p>
 				<p>Email: {user.email}</p>
-				<h2 className="text-2xl font-bold">Usage this period</h2>
-				<p>Cost: {formatCents(costThisPeriod(user))}</p>
-				{user.lastCharged && (
-					<p>Charged on {formatDate(nextMonth(user.lastCharged))}</p>
+				<h2 className="max-w-max text-2xl font-bold border-b-2 border-gray-500">
+					Usage this period
+				</h2>
+				{baseCost <= 0 && (
+					<p className="[&_strong]:text-[#24e098]">
+						You have <strong>{formatCents(-baseCost)}</strong> remaining of your
+						initial <strong>{formatCents(100)}</strong>.
+					</p>
 				)}
-				<h3 className="text-xl font-bold">How we calculate price</h3>
+				<p className="[&_strong]:text-[#24e098]">
+					Upcoming charge: <strong>{formatCents(cost)}</strong>
+					{nextCharge && (
+						<>
+							{' '}
+							on <strong>{formatDate(nextCharge)}</strong>
+						</>
+					)}
+				</p>
+				<h3 className="max-w-max text-xl font-bold border-b-2 border-gray-500">
+					How we calculate price
+				</h3>
 				<p>
-					1,000 tokens is about 750 words. We charge{' '}
-					{formatCents(COST_PER_1000_PROMPT_TOKENS)} for 1,000 prompt tokens and{' '}
-					{formatCents(COST_PER_1000_COMPLETION_TOKENS)} for 1,000 response
-					tokens.
+					<strong>1,000 tokens</strong> is about <strong>750 words</strong>. We
+					charge <strong>{formatCents(COST_PER_1000_PROMPT_TOKENS)}</strong> for{' '}
+					<strong>1,000 prompt tokens</strong> and{' '}
+					<strong>{formatCents(COST_PER_1000_COMPLETION_TOKENS)}</strong> for{' '}
+					<strong>1,000 response tokens</strong>.
 				</p>
 				<p>
-					We also charge an extra $0.30 per month with a minimum of $0.50 per
-					month (if you've used this service at all this month).
+					We also charge an <strong>extra $0.30 per month</strong> with a{' '}
+					<strong>minimum of $0.50 per month</strong> (if you've used this
+					service at all this month).
 				</p>
-				<h3 className="text-xl font-bold">Tips for keeping cost down</h3>
+				<h3 className="max-w-max text-xl font-bold border-b-2 border-gray-500">
+					Tips for keeping cost down
+				</h3>
 				<p>
 					Start a new chat whenever possible. Every message in the chat is
 					passed back to ChatGPT as part of the prompt.
--- lib/user/costThisPeriod.ts
+++ lib/user/costThisPeriod.ts
@@ -4,12 +4,17 @@ import {
 	COST_PER_1000_COMPLETION_TOKENS
 } from '@/lib/costPerToken'
 
-const costThisPeriod = (user: User) => {
+/** Can be less than 0 and does not include Stripe fee. */
+export const baseCostThisPeriod = (user: User) => {
 	const totalCost =
 		(user.requestTokens / 1000) * COST_PER_1000_PROMPT_TOKENS +
 		(user.responseTokens / 1000) * COST_PER_1000_COMPLETION_TOKENS
 
-	const costThisPeriod = Math.ceil(totalCost - user.purchasedAmount)
+	return Math.ceil(totalCost - user.purchasedAmount)
+}
+
+const costThisPeriod = (user: User) => {
+	const costThisPeriod = baseCostThisPeriod(user)
 	if (costThisPeriod <= 0) return 0
 
 	// Stripe takes 2.9% + 30 cents per transaction. Minimum charge is 50 cents.
