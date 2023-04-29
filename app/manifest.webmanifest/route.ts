3c669f2cce8303f48390c5f4752869acbc45d5d5
Ken Mueller <kenmueller0@gmail.com>
2023-04-28 19:05:49 -0700

    web manifest

--- app/manifest.webmanifest/route.ts
+++ app/manifest.webmanifest/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 
 import errorFromUnknown from '@/lib/error/fromUnknown'
+import maskableImage from '@/assets/maskable.png'
 
 export const dynamic = 'force-dynamic'
 
@@ -8,14 +9,24 @@ export const GET = async () => {
 	try {
 		return NextResponse.json(
 			{
-				background_color: 'black',
-				description: 'TryGPT',
-				display: 'standalone',
-				lang: 'en',
+				dir: 'ltr',
+				lang: 'en-US',
+				scope: '/',
+				start_url: '/',
 				name: 'TryGPT',
 				short_name: 'TryGPT',
-				start_url: '/',
-				orientation: 'portrait'
+				description: 'TryGPT',
+				display: 'standalone',
+				theme_color: '#27272a',
+				background_color: '#27272a',
+				categories: ['games', 'social'],
+				icons: [
+					{
+						src: maskableImage.src,
+						sizes: '512x512',
+						purpose: 'maskable any'
+					}
+				]
 			},
 			{
 				headers: {
