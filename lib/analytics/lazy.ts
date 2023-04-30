d3377c53c8adf469750e599bc4e0582d5b56f537
Ken Mueller <kenmueller0@gmail.com>
2023-04-30 09:59:10 -0700

    import .

--- lib/analytics/lazy.ts
+++ lib/analytics/lazy.ts
@@ -1,11 +1,7 @@
 export const setUserId: typeof import('.').setUserId = (...args) => {
-	import('./index')
-		.then(({ setUserId }) => setUserId(...args))
-		.catch(console.error)
+	import('.').then(({ setUserId }) => setUserId(...args)).catch(console.error)
 }
 
 export const logEvent: typeof import('.').logEvent = (...args) => {
-	import('./index')
-		.then(({ logEvent }) => logEvent(...args))
-		.catch(console.error)
+	import('.').then(({ logEvent }) => logEvent(...args)).catch(console.error)
 }
