21745e84854473f2cd335fd961bde86e22d75fea
Ken Mueller <kenmueller0@gmail.com>
2023-04-29 18:31:17 -0700

    middleware redirect to origin

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,39 @@
+if (!process.env.HOST) throw new Error('Missing HOST')
+
 import { NextRequest, NextResponse } from 'next/server'
+
 import DEV from './lib/dev'
+import HttpError from './lib/error/http'
+import ErrorCode from './lib/error/code'
+import errorFromUnknown from './lib/error/fromUnknown'
+
+const ORIGIN = `https://${process.env.HOST}`
 
 const middleware = (request: NextRequest) => {
-	const headers = new Headers(request.headers)
-	const url = new URL(request.url)
+	try {
+		const headers = new Headers(request.headers)
+		const url = new URL(request.url)
 
-	console.log(url.href, Object.fromEntries(headers.entries()))
+		const protocol = headers.get('x-forwarded-proto')
+		const host = headers.get('x-forwarded-host')
 
-	headers.set(
-		'x-url',
-		new URL(url.pathname, DEV ? url.origin : 'https://trygpt.co').href
-	)
+		if (!(protocol && host))
+			throw new HttpError(ErrorCode.BadRequest, 'Invalid request')
 
-	return NextResponse.next({
-		request: { headers }
-	})
+		console.log(protocol, host)
+
+		if (!(DEV || (protocol === 'https' && host === process.env.HOST!)))
+			return NextResponse.redirect(new URL(url.pathname, ORIGIN))
+
+		headers.set('x-url', new URL(url.pathname, DEV ? url.origin : ORIGIN).href)
+
+		return NextResponse.next({
+			request: { headers }
+		})
+	} catch (unknownError) {
+		const { code, message } = errorFromUnknown(unknownError)
+		return new NextResponse(message, { status: code })
+	}
 }
 
 export default middleware
