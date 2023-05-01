9b26669243d05d24293b23fc91d7cb11c65b89ed
Ken Mueller <kenmueller0@gmail.com>
2023-05-01 15:12:23 -0700

    strong text in header

--- components/Landing/Header.tsx
+++ components/Landing/Header.tsx
@@ -9,7 +9,8 @@ const LandingPageHeader = () => (
 			ChatGPT <strong>4</strong> for <strong>$1</strong>
 		</h1>
 		<p className="mt-4 text-center">
-			TryGPT is a pay-as-you-go service for ChatGPT 4. Start now for only $1.
+			TryGPT is a <strong>pay-as-you-go</strong> service for{' '}
+			<strong>ChatGPT 4</strong>. Start now for only <strong>$1</strong>.
 		</p>
 		<Image
 			className="w-full h-auto mt-8 bg-zinc-800 rounded-2xl shadow-lg"
