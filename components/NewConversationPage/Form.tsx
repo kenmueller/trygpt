689de0f768ab1e14c3a3fae0b579a0889bce2792
Ken Mueller <kenmueller0@gmail.com>
2023-05-03 09:23:56 -0700

    form preview

--- components/NewConversationPage/Form.tsx
+++ components/NewConversationPage/Form.tsx
@@ -14,6 +14,7 @@ import {
 import TextAreaAutosize from 'react-textarea-autosize'
 import { useRecoilValue } from 'recoil'
 import debounce from 'lodash/debounce'
+import cx from 'classnames'
 
 import DEV from '@/lib/dev'
 import errorFromUnknown from '@/lib/error/fromUnknown'
@@ -23,6 +24,7 @@ import errorFromResponse from '@/lib/error/fromResponse'
 import { ChatWithUserData } from '@/lib/chat'
 import userState from '@/lib/atoms/user'
 import ThreeDotsLoader from '@/components/ThreeDotsLoader'
+import Markdown from '@/components/Markdown'
 
 const urlMatch = new RegExp(
 	`^(?:${DEV ? 'http' : 'https'}:\\/\\/)?${process.env.NEXT_PUBLIC_HOST.replace(
@@ -125,51 +127,57 @@ const NewConversationPageForm = () => {
 	}, [setChat, setChatError, trimmedUrl, loadChatIdDebounced])
 
 	return (
-		<form
-			className="flex flex-col items-stretch gap-4 px-6 py-4"
-			onSubmit={onSubmit}
-		>
-			<div className="flex justify-between items-center">
-				<h1 className="text-2xl font-bold">Create Conversation</h1>
-				<button
-					className="px-4 py-2 font-bold bg-sky-500 rounded-lg transition-opacity ease-linear hover:opacity-70 disabled:opacity-50"
-					disabled={disabled}
-				>
-					Submit
-				</button>
-			</div>
-			<TextAreaAutosize
-				className="h-[46px] overflow-hidden resize-none px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
-				placeholder="Title"
-				value={title}
-				onChange={onTitleChange}
-			/>
-			<TextAreaAutosize
-				className="h-[93.2px] overflow-hidden resize-none px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
-				placeholder="Description"
-				minRows={3}
-				value={text}
-				onChange={onTextChange}
-			/>
-			<div className="flex flex-col items-stretch gap-2">
-				<input
-					className="px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
-					placeholder={`${DEV ? 'http' : 'https'}://${process.env
-						.NEXT_PUBLIC_HOST!}/chats/{CHAT_ID}`}
-					value={url}
-					onChange={onUrlChange}
+		<div className="flex flex-col items-stretch gap-6 px-6 py-4">
+			<form className="flex flex-col items-stretch gap-4" onSubmit={onSubmit}>
+				<div className="flex justify-between items-center">
+					<h1 className="text-2xl font-bold">New Conversation</h1>
+					<button
+						className="px-4 py-2 font-bold bg-sky-500 rounded-lg transition-opacity ease-linear hover:opacity-70 disabled:opacity-50"
+						disabled={disabled}
+					>
+						Submit
+					</button>
+				</div>
+				<TextAreaAutosize
+					className="h-[46px] overflow-hidden resize-none px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
+					placeholder="Title"
+					value={title}
+					onChange={onTitleChange}
 				/>
-				{trimmedUrl && !(chat || chatError) && (
-					<p>
-						<ThreeDotsLoader />
-					</p>
-				)}
-				{chat && <p className="font-bold text-green-500">{chat.name}</p>}
-				{chatError && (
-					<p className="font-bold text-red-500">{chatError.message}</p>
-				)}
+				<TextAreaAutosize
+					className="h-[93.2px] overflow-hidden resize-none px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
+					placeholder="Description"
+					minRows={3}
+					value={text}
+					onChange={onTextChange}
+				/>
+				<div className="flex flex-col items-stretch gap-2">
+					<input
+						className="px-4 py-[0.7rem] bg-white bg-opacity-10 rounded-lg outline-none placeholder:text-white placeholder:opacity-50"
+						placeholder={`${DEV ? 'http' : 'https'}://${process.env
+							.NEXT_PUBLIC_HOST!}/chats/{CHAT_ID}`}
+						value={url}
+						onChange={onUrlChange}
+					/>
+					{trimmedUrl && !(chat || chatError) && (
+						<p>
+							<ThreeDotsLoader />
+						</p>
+					)}
+					{chat && <p className="font-bold text-green-500">{chat.name}</p>}
+					{chatError && (
+						<p className="font-bold text-red-500">{chatError.message}</p>
+					)}
+				</div>
+			</form>
+			<h2 className="pb-1 border-b border-white border-opacity-50">Preview</h2>
+			<div className="self-center max-w-[1000px] w-full flex flex-col items-stretch gap-4">
+				<h1 className={cx('text-white', !trimmedTitle && 'text-opacity-50')}>
+					{trimmedTitle || 'Title'}
+				</h1>
+				<Markdown text={trimmedText} />
 			</div>
-		</form>
+		</div>
 	)
 }
 
